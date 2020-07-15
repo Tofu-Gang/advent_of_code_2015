048c0dd826f515bbf4fb85ddd831ef96a2475bdb
Tofu Gang <tofugangsw@gmail.com>
2020-07-15 16:48:09 +0200

    Day 1: Puzzle 2 solved.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from sys import exit
-from src.day_01.puzzle import puzzle_01
+from src.day_01.puzzle import puzzle_02
+
+"""
+Santa was hoping for a white Christmas, but his weather machine's "snow" 
+function is powered by stars, and he's fresh out! To save Christmas, he needs 
+you to collect fifty stars by December 25th.
+
+Collect stars by helping Santa solve puzzles. Two puzzles will be made available 
+on each day in the Advent calendar; the second puzzle is unlocked when you 
+complete the first. Each puzzle grants one star. Good luck!
+"""
 
 ################################################################################
 
@@ -8,7 +18,7 @@ if __name__ == "__main__":
     Runs specified puzzles.
     """
 
-    puzzle_01()
+    puzzle_02()
     exit(0)
 
 ################################################################################
--- src/day_01/puzzle.py
+++ src/day_01/puzzle.py
@@ -1,4 +1,6 @@
 """
+--- Day 1: Not Quite Lisp ---
+
 Santa is trying to deliver presents in a large apartment building, but he can't
 find the right floor - the directions he got are a little confusing. He starts
 on the ground floor (floor 0) and then follows the instructions one character at
@@ -16,9 +18,8 @@ never find the top or bottom floors.
 def puzzle_01() -> None:
     """
     Use the puzzle input txt file. To what floor do the instructions take Santa?
-    Answer should be 232.
 
-    :return: None
+    :return: None; Answer should be 232.
     """
 
     floor = 0
@@ -29,5 +30,35 @@ def puzzle_01() -> None:
             elif literal == ")":
                 floor -= 1
     print(floor)
+
+################################################################################
+
+def puzzle_02() -> None:
+    """
+    Now, given the same instructions, find the position of the first character
+    that causes him to enter the basement (floor -1). The first character in the
+    instructions has position 1, the second character has position 2, and so on.
+
+    What is the position of the character that causes Santa to first enter the
+    basement?
+
+    :return: None; Answer should be 1783.
+    """
+
+    floor = 0
+    with open("src/day_01/input.txt", "r") as input:
+        input_string = input.read()
+        for i in range(len(input_string)):
+            literal = input_string[i]
+            if literal == "(":
+                floor += 1
+            elif literal == ")":
+                floor -= 1
+
+            if floor < 0:
+                print(i + 1)
+                break
+        else:
+            print("Basement not reached.")
 
 ################################################################################
