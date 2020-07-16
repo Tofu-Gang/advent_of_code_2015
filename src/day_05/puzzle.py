91e265e592c889c6eb314a74efe2e26bc964e06b
Tofu Gang <tofugangsw@gmail.com>
2020-07-16 22:21:17 +0200

    Day 5: Puzzle 2 solved.

--- src/day_05/puzzle.py
+++ src/day_05/puzzle.py
@@ -10,7 +10,7 @@ FORBIDDEN = ("ab", "cd", "pq", "xy")
 
 ################################################################################
 
-def _is_string_nice(input_string: str) -> bool:
+def _is_string_nice_part1(input_string: str) -> bool:
     """
     A nice string is one with all of the following properties:
     -It contains at least three vowels (aeiou only),
@@ -34,6 +34,31 @@ def _is_string_nice(input_string: str) -> bool:
 
 ################################################################################
 
+def _is_string_nice_part2(input_string: str) -> bool:
+    """
+    Now, a nice string is one with all of the following properties:
+    -It contains a pair of any two letters that appears at least twice in the
+     string without overlapping,
+     like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+    -It contains at least one letter which repeats with exactly one letter
+     between them,
+     like xyx, abcdefeghi (efe), or even aaa.
+
+    :param input_string: the naughty or nice input string
+    :return: True if the input string is nice, False if it is naughty
+    """
+
+    condition_1 = any([
+        input_string.count(input_string[i]+input_string[i + 1]) > 1
+        and (input_string[i] != input_string[i + 1]
+             or input_string[i + 1] != input_string[i + 2])
+        for i in range(len(input_string) - 2)])
+    condition_2 = any([input_string[i] == input_string[i + 2]
+                       for i in range(len(input_string) - 2)])
+    return condition_1 and condition_2
+
+################################################################################
+
 def puzzle_01() -> None:
     """
     How many strings in the input are nice?
@@ -46,7 +71,7 @@ def puzzle_01() -> None:
         count = 0
 
         for line in lines:
-            if _is_string_nice(line.strip()):
+            if _is_string_nice_part1(line):
                 count += 1
 
     print(count)
@@ -55,9 +80,22 @@ def puzzle_01() -> None:
 
 def puzzle_02() -> None:
     """
-    :return: None
+    Realizing the error of his ways, Santa has switched to a better model of
+    determining whether a string is naughty or nice. None of the old rules
+    apply, as they are all clearly ridiculous. How many strings are nice under
+    the new rules?
+
+    :return: None; Answer should be 53.
     """
 
-    pass
+    with open("src/day_05/input.txt", "r") as f:
+        lines = f.readlines()
+        count = 0
+
+        for line in lines:
+            if _is_string_nice_part2(line):
+                count += 1
+
+    print(count)
 
 ################################################################################
