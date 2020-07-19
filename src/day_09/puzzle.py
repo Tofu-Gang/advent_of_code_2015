a070cd61dcf91cb427910f62909cf44d96044a14
Tofu Gang <tofugangsw@gmail.com>
2020-07-19 14:14:07 +0200

    Day 9: Puzzle 2 solved.

--- src/day_09/puzzle.py
+++ src/day_09/puzzle.py
@@ -1,8 +1,15 @@
 from itertools import permutations
-from sys import maxsize
+from typing import Dict
 
 """
 --- Day 9: All in a Single Night ---
+
+Every year, Santa manages to deliver all of his presents in a single night.
+
+This year, however, he has some new locations to visit; his elves have provided 
+him the distances between every pair of locations. He can start and end at any 
+two (different) locations he wants, but he must visit each location exactly 
+once.
 """
 
 LOCATIONS_DENOMINATOR = "to"
@@ -10,17 +17,13 @@ DISTANCE_DENOMINATOR = "="
 
 ################################################################################
 
-def puzzle_01() -> None:
+def _load_distances() -> Dict[str, Dict[str, int]]:
     """
-    Every year, Santa manages to deliver all of his presents in a single night.
+    Loads distances from the puzzle input to a dictionary. In this dictionary,
+    keys are locations and values are dictionaries where keys are neighbour
+    locations and value the distance to this neighbour location.
 
-    This year, however, he has some new locations to visit; his elves have
-    provided him the distances between every pair of locations. He can start and
-    end at any two (different) locations he wants, but he must visit each
-    location exactly once. What is the shortest distance he can travel to
-    achieve this?
-
-    :return: None; Answer should be 117.
+    :return:
     """
 
     with open("src/day_09/input.txt", "r") as f:
@@ -42,18 +45,41 @@ def puzzle_01() -> None:
                 distances[location2].update({location1: distance})
             except KeyError:
                 distances[location2] = {location1: distance}
+        return distances
 
-        print(min([sum([distances[perm[i]][perm[i + 1]]
-                        for i in range(len(perm) - 1)])
-                   for perm in permutations(distances.keys())]))
+################################################################################
+
+def puzzle_01() -> None:
+    """
+    What is the shortest distance Santa can travel to visit each location
+    exactly once?
+
+    :return: None; Answer should be 117.
+    """
+
+    distances = _load_distances()
+    print(min([sum([distances[perm[i]][perm[i + 1]]
+                    for i in range(len(perm) - 1)])
+               for perm in permutations(distances.keys())]))
 
 ################################################################################
 
 def puzzle_02() -> None:
     """
-    :return: None
+    The next year, just to show off, Santa decides to take the route with the
+    longest distance instead.
+
+    He can still start and end at any two (different) locations he wants, and he
+    still must visit each location exactly once.
+
+    What is the distance of the longest route?
+
+    :return: None; Answer should be 909.
     """
 
-    pass
+    distances = _load_distances()
+    print(max([sum([distances[perm[i]][perm[i + 1]]
+                    for i in range(len(perm) - 1)])
+               for perm in permutations(distances.keys())]))
 
 ################################################################################
