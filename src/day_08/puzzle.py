3efbf2e67ae4c47105dd0687af796482e2eebd82
Tofu Gang <tofugangsw@gmail.com>
2020-07-19 13:08:04 +0200

    Day 8: Puzzle 1 solved.

---
+++ src/day_08/puzzle.py
@@ -0,0 +1,46 @@
+"""
+--- Day 8: Matchsticks ---
+Space on the sleigh is limited this year, and so Santa will be bringing his list
+as a digital copy. He needs to know how much space it will take up when stored.
+
+It is common in many programming languages to provide a way to escape special
+characters in strings. For example, C, JavaScript, Perl, Python, and even PHP
+handle special characters in very similar ways.
+
+However, it is important to realize the difference between the number of
+characters in the code representation of the string literal and the number of
+characters in the in-memory string itself.
+"""
+
+################################################################################
+
+def puzzle_01() -> None:
+    """
+    Santa's list is a file that contains many double-quoted string literals, one
+    on each line. The only escape sequences used are \\ (which represents a
+    single backslash), \" (which represents a lone double-quote character), and
+    \ x plus two hexadecimal characters (which represents a single character
+    with that ASCII code).
+
+    Disregarding the whitespace in the file, what is the number of characters of
+    code for string literals minus the number of characters in memory for the
+    values of the strings in total for the entire file?
+
+    :return: None; Answer should be 1350.
+    """
+
+    with open("src/day_08/input.txt", "r") as f:
+        lines = f.readlines()
+        print(sum([len(line.strip()) - len(eval(line.strip()))
+                   for line in lines]))
+
+################################################################################
+
+def puzzle_02() -> None:
+    """
+    :return: None
+    """
+
+    pass
+
+################################################################################
