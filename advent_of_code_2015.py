06d254a29b34aa6d23606206cf2597512720e71f
Jakub Franěk <tofugangsw@gmail.com>
2022-10-03 14:36:33 +0200

    Initial commit.

---
+++ advent_of_code_2015.py
@@ -0,0 +1,23 @@
+__author__ = "Jakub Franěk"
+__email__ = "tofugangsw@gmail.com"
+
+"""
+Santa was hoping for a white Christmas, but his weather machine's "snow" 
+function is powered by stars, and he's fresh out! To save Christmas, he needs 
+you to collect fifty stars by December 25th.
+
+Collect stars by helping Santa solve puzzles. Two puzzles will be made available 
+on each day in the Advent calendar; the second puzzle is unlocked when you 
+complete the first. Each puzzle grants one star. Good luck!
+"""
+
+################################################################################
+
+if __name__ == "__main__":
+    """
+    Runs specified puzzles.
+    """
+
+    print("Advent of Code 2015")
+
+################################################################################
